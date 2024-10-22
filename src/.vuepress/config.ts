cc3155f7e94b48dfd7e83dff27643541e80b5b46
Xiaolin <xiaolin_daily@163.com>
2024-10-23 00:52:21 +0800

    更新

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,16 @@
+import { defineUserConfig } from "vuepress";
+
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/XiaoLin",
+
+  lang: "zh-CN",
+  title: "小林博客",
+  description: "vuepress-theme-hope 的博客演示",
+
+  theme,
+
+  // 和 PWA 一起启用
+  // shouldPrefetch: false,
+});
