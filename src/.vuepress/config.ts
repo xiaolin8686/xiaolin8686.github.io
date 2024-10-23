f4643561a95a969f2b9fce564defc7cc727c166a
Xiaolin <xiaolin_daily@163.com>
2024-10-23 08:45:04 +0800

    更改

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/XiaoLin",
+  base: "/",
 
   lang: "zh-CN",
   title: "小林博客",
